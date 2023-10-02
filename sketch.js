d87853f3fb7679badb9db2d3051a0da397cb1e50
MatheusBR23 <145366291+MatheusBR23@users.noreply.github.com>
2023-10-02 11:42:18 -0300

    adicionando arquivos

---
+++ sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+    createCanvas(600,600);
+      background("purple");
+  }
+  
+  function draw() {
+  
+    stroke("lightgreen");
+  fill("darkred");
+    
+      // console.log(mouseIsPressed);
+  
+  if(mouseIsPressed){
+    rect(mouseX, mouseY, 10,-100
+        );
+  }
+  }
